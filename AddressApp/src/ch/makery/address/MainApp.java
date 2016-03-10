bdebd8e9b801f9d0bac5ba80d4b3deb8026dc945
marcelo.c.silva <marcelo.c.silva@EISA-NB160.eisa.corp.com>
2016-03-10 18:17:06 -0300

    criando teste de conceito baseado no tutorial
    http://code.makery.ch/library/javafx-8-tutorial/pt/part1/
    
    ininicar a parte:
    http://code.makery.ch/library/javafx-8-tutorial/pt/part2/

---
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -0,0 +1,58 @@
+package ch.makery.address;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AdressApp");
+
+		initRootLayout();
+		
+		showPersonOverview();
+	}
+
+	private void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void showPersonOverview(){
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(personOverview);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Stage getPrimaryStage(){
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
